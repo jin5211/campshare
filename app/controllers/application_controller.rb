b92377c091a6e6bcd968c7edd60fd4afed5ff45a
Jin <zinkoko5211@gmail.com>
2024-05-21 23:19:30 +0900

    deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :profile_image, :name, :profile, :prefecture_id, :privacy_policy, :address, :phone_number, :contact_time_id, :contact_time_another])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
