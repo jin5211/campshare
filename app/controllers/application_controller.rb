6c99cbd935fcc4624dfd5f5d1dab91c06753a24d
Jin <zinkoko5211@gmail.com>
2024-05-28 13:00:59 +0900

    First Test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :profile_image, :name, :profile, :prefecture_id, :privacy_policy, :address, :phone_number, :contact_time_id, :contact_time_another])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:profile_image, :name, :profile, :prefecture_id, :privacy_policy, :address, :phone_number, :contact_time_id, :contact_time_another])
   end
 
   def basic_auth
